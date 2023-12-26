87d118ebb44efe224e3ca5e7e3151d9c48070b69
adam-woolhether <114398282+adam-woolhether@users.noreply.github.com>
2023-12-26 19:22:18 +0800

    go day 2

---
+++ go/days/day02/main.go
@@ -0,0 +1,75 @@
+package day02
+
+import (
+	"bufio"
+	"bytes"
+	"embed"
+	"fmt"
+)
+
+var limits = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+//go:embed games.txt
+var gamesInput embed.FS
+
+func Result() (int, error) {
+	var result int
+
+	data, err := gamesInput.Open("games.txt")
+	if err != nil {
+		return -1, fmt.Errorf("opening calibration document: %w", err)
+	}
+
+	scanner := bufio.NewScanner(data)
+
+	for scanner.Scan() {
+		if scanner.Err() != nil {
+			return -1, fmt.Errorf("scanning calibration document: %w", err)
+		}
+
+		lineBytes := scanner.Bytes()
+
+		result += possibleGames(lineBytes)
+	}
+
+	return result, nil
+}
+
+func possibleGames(line []byte) int {
+	before, after, _ := bytes.Cut(line, []byte(":"))
+
+	results := bytes.Split(after, []byte(";"))
+
+	for i := range results {
+		picks := bytes.Split(results[i], []byte(","))
+
+		for j := range picks {
+			trimmed := bytes.TrimSpace(picks[j])
+			amount, color, _ := bytes.Cut(trimmed, []byte(" "))
+
+			amt := byteToInt(amount)
+
+			if amt > limits[string(color)] {
+				return 0
+			}
+		}
+	}
+
+	successfulAmt, _ := bytes.CutPrefix(before, []byte("Game "))
+
+	return byteToInt(successfulAmt)
+}
+
+func byteToInt(input []byte) int {
+	var result int
+
+	for i := range input {
+		result = result*10 + int(input[i]-'0')
+	}
+
+	return result
+}
--- go/main.go
+++ go/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"aoc2023/days/day01"
+	"aoc2023/days/day02"
 )
 
 func main() {
@@ -14,5 +15,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	day02Result, err := day02.Result()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Day 02 Error: %v\n", err)
+		os.Exit(1)
+	}
+
 	fmt.Printf("Day 01 Result: %d\n", day01Result)
+	fmt.Printf("Day 02 Result: %d\n", day02Result)
 }
