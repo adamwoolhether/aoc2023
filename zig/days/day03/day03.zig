eb0e48214f8c0d14df50d898877a72288899d7ca
adam-woolhether <114398282+adam-woolhether@users.noreply.github.com>
2024-01-16 16:35:01 +0800

    zig day 3 part 2

--- zig/days/day03/day03.zig
+++ zig/days/day03/day03.zig
@@ -5,7 +5,7 @@ const isDigit = std.ascii.isDigit;
 
 const day03_result = struct {
     part1: usize,
-    // part2: u64,
+    part2: usize,
 };
 
 pub fn run() anyerror!day03_result {
@@ -35,8 +35,6 @@ pub fn run() anyerror!day03_result {
         grid.deinit();
     }
 
-    // var part2_result: u64 = 0;
-
     while (try reader.readUntilDelimiterOrEof(buffer, '\n')) |line| {
         // std.debug.print("line {s}\n", .{line});
 
@@ -44,14 +42,14 @@ pub fn run() anyerror!day03_result {
         // simply overrridden with the final line in the buffer.
         var lineCopy = try alloc.dupe(u8, line);
         try grid.append(lineCopy);
-        // part2_result += try part2.maxPowerGames(line);
     }
 
     const part1Result = sumParts(grid);
+    const part2Result = sumGearPowers(grid);
 
     const result = day03_result{
         .part1 = part1Result,
-        // .part2 = part2_result,
+        .part2 = try part2Result,
     };
 
     return result;
@@ -113,6 +111,85 @@ fn nextToSymbol(grid: std.ArrayList([]const u8), row: usize, low: usize, high: u
     }
 
     return false;
+}
+
+fn sumGearPowers(grid: std.ArrayList([]const u8)) !usize {
+    var result: usize = 0;
+
+    var gears = std.AutoHashMap([2]usize, usize).init(allocator);
+    defer gears.deinit();
+
+    for (grid.items, 0..) |row, rowIdx| {
+        var colIdx: usize = 0;
+
+        while (colIdx < row.len) : (colIdx += 1) {
+            if (isDigit(row[colIdx])) {
+                var start: usize = colIdx;
+                var end: usize = colIdx;
+
+                while (end < row.len) {
+                    if (!isDigit(row[end])) break;
+
+                    end += 1;
+                }
+
+                const check = nextToGear(grid, rowIdx, start, end);
+                if (check.found) {
+                    const num = std.fmt.parseInt(usize, row[start..end], 10) catch 0;
+                    // std.debug.print("found valid gear int: {}\n", .{num});
+                    // std.debug.print("gear loc: {},{}\n", .{ check.row, check.col });
+
+                    var v = try gears.getOrPut([2]usize{ check.row, check.col });
+                    if (!v.found_existing) {
+                        v.value_ptr.* = num;
+                    } else {
+                        var found = v.value_ptr.*;
+                        found *= num;
+
+                        result += found;
+                    }
+                }
+
+                colIdx = end;
+            }
+        }
+    }
+
+    return result;
+}
+
+const loc = struct {
+    found: bool,
+    row: usize,
+    col: usize,
+};
+
+fn nextToGear(grid: std.ArrayList([]const u8), row: usize, low: usize, high: usize) loc {
+    const rowCheck = [3]i32{ -1, 0, 1 };
+
+    for (rowCheck) |delta| {
+        const r = @as(i32, @intCast(row)) + delta;
+        if (r < 0 or r >= grid.items.len) continue;
+
+        var l = std.math.sub(usize, low, 1) catch 0;
+        const checkRow = grid.items[@as(usize, @intCast(r))];
+
+        while (l < high) : (l += 1) {
+            if (isGear(checkRow[l])) {
+                const location = loc{ .found = true, .row = @as(usize, @intCast(r)), .col = l };
+                return location;
+            }
+
+            if (high + 1 > checkRow.len) continue;
+            if (isSymbol(checkRow[high])) {
+                const location = loc{ .found = true, .row = @as(usize, @intCast(r)), .col = high };
+                return location;
+            }
+        }
+    }
+
+    const res = loc{ .found = false, .col = undefined, .row = undefined };
+    return res;
 }
 
 fn isSymbol(char: u8) bool {
--- zig/main.zig
+++ zig/main.zig
@@ -8,9 +8,10 @@ pub fn main() anyerror!void {
     const day02Result = try day02.run();
     const day03Result = try day03.run();
 
-    try std.io.getStdOut().writer().print("Day 01 Part 1 Result: {}\n", .{day01Result.part1});
-    try std.io.getStdOut().writer().print("Day 01 Part 2 Result: {}\n", .{day01Result.part2});
-    try std.io.getStdOut().writer().print("Day 02 Part 1 Result: {}\n", .{day02Result.part1});
-    try std.io.getStdOut().writer().print("Day 02 Part 2 Result: {}\n", .{day02Result.part2});
-    try std.io.getStdOut().writer().print("Day 03 Part 1 Result: {}\n", .{day03Result.part1});
+    try std.io.getStdOut().writer().print("Day 01 Pt 1 Result: {}\n", .{day01Result.part1});
+    try std.io.getStdOut().writer().print("Day 01 Pt 2 Result: {}\n", .{day01Result.part2});
+    try std.io.getStdOut().writer().print("Day 02 Pt 1 Result: {}\n", .{day02Result.part1});
+    try std.io.getStdOut().writer().print("Day 02 Pt 2 Result: {}\n", .{day02Result.part2});
+    try std.io.getStdOut().writer().print("Day 03 Pt 1 Result: {}\n", .{day03Result.part1});
+    try std.io.getStdOut().writer().print("Day 03 Pt 2 Result: {}\n", .{day03Result.part2});
 }
